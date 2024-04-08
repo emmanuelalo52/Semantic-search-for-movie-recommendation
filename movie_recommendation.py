348048151ceefc20d314b947a57ab32a49a3190e
Alo Emmanuel Temitope <emmanuelalo52@gmail.com>
2024-04-08 17:12:30 +0100

    modified:   movie_recommendation.py

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -17,12 +17,35 @@ db = client.sample_mflix
 collection = db.movies
 
 #generate LLM embeddings for posgresdb
-def generate_embeddings(text:str) -> list[float]:
-    response = requests.post(
-        embedding_url,
-        headers={"Authorization": f"Beare {hf_token}"},
-        json={"inputs":text}
-    )
-    if response.status_code != 200:
-        raise ValueError(f"Request failed with status code{response.status_code}:{response.text}")
-    return response.json 
+def generate_embedding(text: str) -> list[float]:
+
+  response = requests.post(
+    embedding_url,
+    headers={"Authorization": f"Bearer {hf_token}"},
+    json={"inputs": text})
+
+  if response.status_code != 200:
+    raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+
+  return response.json()
+
+# for doc in collection.find({'plot':{"$exists":True}}).limit(50):
+#   plot_data = {"source_sentence": doc['plot']}  # Keep the dictionary format
+#   doc['plot_embedding_hf'] = generate_embedding(plot_data)
+#   collection.replace_one({'_id': doc['_id']}, doc)
+
+
+query = "imaginary characters from outer space at war"
+
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PlotSemanticSearch",
+      }}
+])
+
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
